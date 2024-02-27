d4644212a2566cadfe103e8bd7576c39e2843e30
Lucas <lucstopyra@gmail.com>
2024-02-27 23:31:30 +0100

    filmanim

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -4,26 +4,40 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import styles from '../Components/Menu.module.scss';
+import MenuTwoToneIcon from '@mui/icons-material/MenuTwoTone';
+import ReactPlayer from 'react-player';
+
+import video from '../FILM_GASTOP.mp4'
 
 function Menu() {
   return (
     <>
-    <Navbar expand="lg">
-      <Container>
-        <Navbar.Brand href="#home"><img src="logo-gastop.png"/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav fill>
-          {/* <div className={styles.menuRight}> */}
-            <Link className={styles.Linki} to={`Products`}>Products</Link>
-            <Link className={styles.Linki} to={`About`}>About</Link>
-            <Link className={styles.Linki} to={`Portfolio`}>Porftolio</Link>
-            <Link className={styles.Linki} to={`Contact`}>Contact</Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg">
+        <MenuTwoToneIcon sx={{ border: 2, minWidth: 40, borderRadius: 2, fontSize: 60 }} />
+        {/* <ReactPlayer
+        src="../FILM_GASTOP.mp4"
+        controls={true}
+        width="800px"
+        height="450px"/> */}
+
+      <video src={video} width="1200" height="400" muted autoplay></video>
+      <video src={video} width="1200" height="400" controls autoplay />
+
+        <Container>
+          <Navbar.Brand href="#home"><img src="logo-gastop.png" /></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav fill>
+              {/* <div className={styles.menuRight}> */}
+              <Link className={styles.Linki} to={`Products`}>Products</Link>
+              <Link className={styles.Linki} to={`About`}>About</Link>
+              <Link className={styles.Linki} to={`Portfolio`}>Porftolio</Link>
+              <Link className={styles.Linki} to={`Contact`}>Contact</Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
