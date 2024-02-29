8298c01b4c62a2dc4098a92ce91a73a1415ec9b3
Lucas Stop <lucstopyra@gmail.com>
2024-02-29 17:08:40 +0100

    udanenavbar

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,7 +1,4 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import styles from '../Components/Menu.module.scss';
 import DensityMedium from '@mui/icons-material/DensityMedium';
@@ -11,6 +8,8 @@ function Menu() {
 
   function myFunction() {
     var x = document.getElementById("myTopnav");
+    var y = document.getElementById("menuMobile");
+    
       if (x.className === styles.topnav) {
       x.className += styles.responsive;
           } else {
@@ -18,13 +17,11 @@ function Menu() {
     }
   }
 
-
-
   return (
     <>
    
       <div className={styles.topnav} id="myTopnav">
-        <div className={styles.menuMobile}>
+        <div className={styles.menuMobile} id="menuMobile">
           <Link className={styles.active} to={`/`}><img src="logo-gastop.png" /></Link>
           <Link className={styles.Linki} to={`Products`}>Products</Link>
           <Link className={styles.Linki} to={`About`}>About</Link>
@@ -36,37 +33,7 @@ function Menu() {
           <DensityMedium sx={{ border: 2, paddingTop: 1, paddingBottom: 1, minWidth: 50, borderRadius: 2, fontSize: 40 }} />
         </a>
       </div>
-
-
-
-
-
-      <Navbar expand="lg">
-        {/* <ReactPlayer
-        src="../FILM_GASTOP.mp4"
-        controls={true}
-        width="800px"
-        height="450px"/> */}
-
-        {/* <video src={video} width="1200" height="400" muted autoplay></video>
-      <video src={video} width="1200" height="400" controls autoplay /> */}
-
-        {/* <Container>
-          <Navbar.Brand href="#home"><img src="logo-gastop.png" /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav fill>
-              {/* <div className={styles.menuRight}> */}
-        {/* <Link className={styles.Linki} to={`Products`}>Products</Link>
-              <Link className={styles.Linki} to={`About`}>About</Link>
-              <Link className={styles.Linki} to={`Portfolio`}>Porftolio</Link>
-              <Link className={styles.Linki} to={`Contact`}>Contact</Link> */}
-
-        {/* </Nav> */}
-        {/* </Navbar.Collapse> */}
-        {/* </Container> */}
-      </Navbar>
-    </>
+   </>
   );
 }
 
---
+++ src/Pages/Probny.js
@@ -0,0 +1,29 @@
+import ReactPlayer from 'react-player';
+import video from '../FILM_GASTOP.mp4'
+
+
+<Navbar expand="lg">
+{/* <ReactPlayer
+src="../FILM_GASTOP.mp4"
+controls={true}
+width="800px"
+height="450px"/> */}
+
+{/* <video src={video} width="1200" height="400" muted autoplay></video>
+<video src={video} width="1200" height="400" controls autoplay /> */}
+
+{/* <Container>
+  <Navbar.Brand href="#home"><img src="logo-gastop.png" /></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav fill>
+      {/* <div className={styles.menuRight}> */}
+{/* <Link className={styles.Linki} to={`Products`}>Products</Link>
+      <Link className={styles.Linki} to={`About`}>About</Link>
+      <Link className={styles.Linki} to={`Portfolio`}>Porftolio</Link>
+      <Link className={styles.Linki} to={`Contact`}>Contact</Link> */}
+
+{/* </Nav> */}
+{/* </Navbar.Collapse> */}
+{/* </Container> */}
+</Navbar>
