9f25352f054e075e91050e75aac9441e287b6234
Lucas <lucstopyra@gmail.com>
2024-03-02 13:08:29 +0100

    poprawny navbar

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+
 import styles from '../Components/Menu.module.scss';
 import DensityMedium from '@mui/icons-material/DensityMedium';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
@@ -6,16 +7,19 @@ import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 import PortraitIcon from '@mui/icons-material/Portrait';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 
-let IconProducts = <AutoStoriesIcon sx={{ paddingBottom: 1, fontSize: 50, opacity: 0.3, marginRight: 2 }} />;
-let IconContact = <PhoneAndroidIcon sx={{ paddingBottom: 1, fontSize: 50, opacity: 0.3, marginRight: 2 }} />;
-let IconAbout = <PortraitIcon sx={{ paddingBottom: 1, fontSize: 50, opacity: 0.3, marginRight: 2 }} />;
-let IconPortfolio = <ThumbUpAltIcon sx={{ paddingBottom: 1, fontSize: 50, opacity: 0.3, marginRight: 2 }} />;
+let IconProducts = <AutoStoriesIcon className={styles.iconItem}/>;
+let IconContact = <PhoneAndroidIcon className={styles.iconItem}/>;
+let IconAbout = <PortraitIcon className={styles.iconItem}/>;
+let IconPortfolio = <ThumbUpAltIcon className={styles.iconItem}/>;
+
+// console.log("Iconproducts: ",IconProducts)
 
 
 function Menu() {
 
-    function myFunction(){
-        console.log('my function triggered');
+    function toggleVisibility(){
+        let x = document.getElementById('linksContainer');
+        x.classList.toggle(styles.makeVisible);  
     } 
 
 
@@ -26,14 +30,16 @@ function Menu() {
                 <div className={styles.logoContainer}>
                     <Link className={styles.icon} to={`/`}><img src="logo-gastop.png" /></Link>
                 </div>
-                <div className={styles.linksContainer}>
-                    <Link className={styles.Linki} to={`Products`}>{IconProducts}Products</Link>
-                    <Link className={styles.Linki} to={`About`}>{IconAbout}About</Link>
-                    <Link className={styles.Linki} to={`Portfolio`}>{IconPortfolio}Porftolio</Link>
-                    <Link className={styles.Linki} to={`Contact`}>{IconContact}Contact</Link>
+                <div className={styles.linksContainer} id="linksContainer">
+
+                    <Link className={styles.Linki} onClick={toggleVisibility} to={`Products`}>{IconProducts}Products</Link>
+                    <Link className={styles.Linki} onClick={toggleVisibility} to={`About`}>{IconAbout}About</Link>
+                    <Link className={styles.Linki} onClick={toggleVisibility} to={`Portfolio`}>{IconPortfolio}Porftolio</Link>
+                    <Link className={styles.Linki} onClick={toggleVisibility} to={`Contact`}>{IconContact}Contact</Link>
+
                 </div>
                 <div className={styles.iconButtonContainer}>
-                    <a href="javascript:void(0);" className={styles.icon} onClick={myFunction}>
+                    <a href="javascript:void(0);" className={styles.iconButton} onClick={toggleVisibility}>
                         <DensityMedium sx={{ border: 2, paddingTop: 1, paddingBottom: 1, minWidth: 50, borderRadius: 2, fontSize: 40 }} />
                     </a>
                 </div>
