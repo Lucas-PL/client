3d67e9e60e48a2febcbbb3d1a4c66a14690d87dc
Lucas <lucstopyra@gmail.com>
2024-03-16 18:10:49 +0100

    aboutfonts

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,21 +1,25 @@
 import Menu from '../Components/Menu';
-import PageTitle from '../Components/PageTitle';
+
 import VisitCard from '../Components/VisitCard';
 import MainTitle from '../Components/MainTitle';
+import Typography from '@mui/material/Typography';
 
 export default function About() {
 
-    return (
-      <>
+  return (
+    <>
       <div className="App">
-      <MainTitle 
-        titleOne="Our" 
-        titleTwo="History" 
-        text="We believe that the idea supported by passion, work, and engagement give a result in the form of a refined product, satisfaction and user safety."/>
-    
+
+        <MainTitle
+          titleOne="Our"
+          titleTwo="History"
+          text="We believe that the idea supported by passion, work, and engagement give a result in the form of a refined product, satisfaction and user safety." />
+
       </div>
-      <VisitCard name="Eryk Batorowicz"/>
-      <VisitCard name="Tomasz Zieliński"/>
-      </>
-    )
-  }
+      <Typography>
+        <VisitCard name="Eryk Batorowicz" />
+        <VisitCard name="Tomasz Zieliński" />
+      </Typography>
+    </>
+  )
+}
--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,5 +1,9 @@
 import { Outlet, Link } from "react-router-dom";
 import Menu from "../Components/Menu";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const Layout = () => {
   return (
